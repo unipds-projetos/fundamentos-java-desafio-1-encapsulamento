594830ab7f08ed38d7c162531986c90c82f32eb5
Mauricio Osmainchi Da Rosa <tp.mauricio.or@totvspartners.com.br>
2025-07-08 21:05:17 -0300

    Adicionado validação básica dos parametros da classe produto

--- src/main/java/br/com/unipds/fundamentosjava/desafio/Main.java
+++ src/main/java/br/com/unipds/fundamentosjava/desafio/Main.java
@@ -8,13 +8,13 @@ public class Main {
         Produto produto1 = new Produto(101, "Caneta Azul", 1.50, 100);
         System.out.println("Produto 1 (Inicial): " + produto1);
 
-        produto1.preco = -2.00;
+        produto1.setPreco(-2.00);
         System.out.println("Produto 1 (Preço alterado): " + produto1);
 
         Produto produto2 = new Produto(102, "Azul Caneta", 15.00, 50);
         System.out.println("Produto 2 (Inicial): " + produto2);
 
-        produto2.quantidadeEmEstoque = -5;
+        produto2.setQuantidadeEmEstoque(-5);
         System.out.println("Produto 2 (Estoque alterado): " + produto2);
 
         Produto produto3 = new Produto(103, "Borracha Branca", -0.50, 0);
--- src/main/java/br/com/unipds/fundamentosjava/desafio/Produto.java
+++ src/main/java/br/com/unipds/fundamentosjava/desafio/Produto.java
@@ -2,20 +2,53 @@ package br.com.unipds.fundamentosjava.desafio;
 
 public class Produto {
 
-    public int id;
-    public String nome;
-    public double preco;
-    public int quantidadeEmEstoque;
+	private int id;
+	private String nome;
+	private double preco;
+	private int quantidadeEmEstoque;
 
-    public Produto(int id, String nome, double preco, int quantidadeEmEstoque) {
-        this.id = id;
-        this.nome = nome;
-        this.preco = preco;
-        this.quantidadeEmEstoque = quantidadeEmEstoque;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    @Override
-    public String toString() {
-        return "Produto [ID=" + id + ", Nome=" + nome + ", Preço=" + preco + ", Estoque=" + quantidadeEmEstoque + "]";
-    }
+	public void setNome(String nome) {
+		if(nome == null || nome.trim().isEmpty()) {
+			throw new IllegalArgumentException("O nome do produto deve ser preenchido");
+		}
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		if (preco < 0) {
+			throw new IllegalArgumentException("O preço não pode ser negativo");
+		}
+		this.preco = preco;
+	}
+
+	public int getQuantidadeEmEstoque() {
+		return quantidadeEmEstoque;
+	}
+
+	public void setQuantidadeEmEstoque(int quantidadeEmEstoque) {
+		if (quantidadeEmEstoque < 0) {
+			throw new IllegalArgumentException("A quantidade em estoque não pode ser negativa");
+		}
+		this.quantidadeEmEstoque = quantidadeEmEstoque;
+	}
+
+	public Produto(int id, String nome, double preco, int quantidadeEmEstoque) {
+		this.id = id;
+		setNome(nome);
+		setPreco(preco);
+		setQuantidadeEmEstoque(quantidadeEmEstoque);
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [ID=" + id + ", Nome=" + nome + ", Preço=" + preco + ", Estoque=" + quantidadeEmEstoque + "]";
+	}
 }
